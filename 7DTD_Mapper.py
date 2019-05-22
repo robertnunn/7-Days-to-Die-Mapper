1da31cbc0264a8b660cec2d54acf73948bfd33ee
Rob <robnunn10@gmail.com>
2019-05-21 22:49:50 -0400

    Updated some variable names, sync with local env

--- 7DTD_Mapper.py
+++ 7DTD_Mapper.py
@@ -8,30 +8,32 @@ logging.basicConfig(level=logging.DEBUG, filename='mapping.log', filemode='w')
 
 # takes the POI coords and gets the coords for the label
 def marker_coords(char, x_coord, y_coord, font):
-  size = font.getsize(char) # gets a tuple with size of text in pixels (X, Y) (X is horiz, Y is vert)
-  x_offset = size[0] // 2 # (integer division)
-  y_offset = size[1] // 2 # (integer division)
-  coords = (x_coord - x_offset, y_coord - y_offset) # origin (0,0) is upper left corner
+  size = font.getsize(char)  # gets a tuple with size of text in pixels (X, Y) (X is horiz, Y is vert)
+  x_offset = size[0] // 2  # (integer division)
+  y_offset = size[1] // 2  # (integer division)
+  coords = (x_coord - x_offset, y_coord - y_offset)  # origin (0,0) is upper left corner
   return coords
 
 
 # convert between in-game coordinates and pillow coordinates
+# in-game coords have their origin at the center, pillow coords have their origin in upper left corner
 def convert_coords(map_size, in_game_coords):
     x, y = in_game_coords
-    xloc = (map_size//2) + x
+    xloc = (map_size//2) + x  # maps are hi-res enough that these rounding errors are negligible
     yloc = (map_size//2) - y
     return (xloc, yloc)
 
 
 # X is EW, Y is NS
 # gets the values from the location string of a POI instance
+# returns a tuple
 def extract_xy(text):
     xpos = text.find(',')
     ypos = text.rfind(',') + 1
     return (int(text[:xpos]), int(text[ypos:]))
 
 
-# takes the in-game, internal coordinates and formats them to be NS EW coordinates
+# takes the in-game, internal coordinates (tuple) and formats them to be NS EW coordinates (returns a string)
 def pretty_in_game_coords(coord_tuple):
     x, y = coord_tuple
     if x > 0:
@@ -47,32 +49,34 @@ def pretty_in_game_coords(coord_tuple):
 
 # do the thing
 def main(folder):
+
     # get biome and road maps
     biomes = Image.open(folder + '\\biomes.png').convert('RGBA')
     roads = Image.open(folder + '\\splat3.png').convert('RGBA')
-    out = Image.alpha_composite(biomes, roads)  # 16k maps require changing the MAX_IMAGE_PIXELS parameter on line 84 of PIL/Image.py (goto alpha_comp declaration, scroll up)
-    out.save(folder + '\\' + folder + ' no markers.png', 'png')  # this needs to set the filename to the folder name, folder name should be the seed used
+    out = Image.alpha_composite(biomes, roads)
+    out.save(folder + '\\' + folder + ' no markers.png', 'png')  # this needs to set the filename to the folder name
     # out = Image.open('copy.png')
     map_size = out.size[0]
     logging.debug("map_size: " + str(map_size))
 
     pretty_name_dict = dict()
-    with open('prefab_lookup.csv') as lookup_csv:  # open the file, read it, split it into a list of lines, typecast to set
+    with open('prefab_lookup.csv') as lookup_csv:  # open the file, read it, split it into a list of lines, typecast to set to remove duplicates
         prefab_text = lookup_csv.read()
         prefab_set = set(prefab_text.split('\n'))
     try:
         prefab_set.remove('')  # remove empty string from set
     except:
         pass
+
     prefab_list = list(prefab_set)  # back to the more comfortable list
     for i in prefab_list:
         in_name, nice_name = i.split(',')
-        pretty_name_dict[in_name] = nice_name # ########################
+        pretty_name_dict[in_name] = nice_name  # create name-pretty name lookup dict
     logging.debug("pretty name keys")
     logging.debug(pretty_name_dict.keys())
     logging.debug("pretty name values")
     logging.debug(pretty_name_dict.values())
-    reverse_pretty_name_dict = dict((v,k) for k,v in pretty_name_dict.items())  # #######################
+    reverse_pretty_name_dict = dict((v, k) for k, v in pretty_name_dict.items())  # #######################
 
     # this block of code pulls all the individual POIs out of the XML file
     filename = folder + '\\prefabs.xml'
@@ -110,7 +114,7 @@ def main(folder):
     d = ImageDraw.Draw(out)  # get a drawing context
     legend = ['Marker\tPOI Name (count)']  # write header row
     for i in range(len(pretty_search_list)):
-        marker = str(i) # POIs are listed by number, lookup table in a text file
+        marker = str(i)  # POIs are listed by number, lookup table in a text file
         search_term = pretty_search_list[i]
         leg_string = str(i) + "\t" + search_term + " (" + str(len(results[reverse_pretty_name_dict[search_term]])) + ")"
         if len(results[reverse_pretty_name_dict[search_term]]) == 1:
@@ -134,7 +138,9 @@ if __name__ == "__main__":
     folders = args[1:]
     logging.debug(folders)
 
-    for i in folders:
-        print('Processing ' + i)
-        logging.debug('Processing ' + i)
-        main(i)
+    Image.MAX_IMAGE_PIXELS = None  # this is necessary for 16k maps, otherwise decompression bomb-prevention kicks in and cancels it
+
+    for name in folders:
+        print('Processing ' + name)
+        logging.debug('Processing ' + name)
+        main(name)
